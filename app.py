e9c91e9a17fea61b04026a47fe184ec9abccc49b
krishanharwani <krishanharwani1@gmail.com>
2022-04-06 20:24:05 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,81 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import plotly.express as px
+import joblib
+import base64
+from pandas import MultiIndex, Int64Index
+
+
+
+
+data = pd.read_csv('https://wqp.herokuapp.com/getCSV/')
+data.columns = ["ph","Turbidity"]
+original_data = data.copy(deep=True)
+data = data.iloc[:,0:]
+# original_data = pd.read_csv('https://wqp.herokuapp.com/getCSV/')
+feature_list = {'ph':0,'Turbidity':0,'pH_Type':0,'Turbidity_Type':0}
+classifier = joblib.load('algorithms.sav')
+rfc=joblib.load('rfclassifier.sav')
+dtc=joblib.load('dtclassifier.sav')
+xgbc=joblib.load('xgbclassifier.sav')
+gbc=joblib.load('gbclassifier.sav')
+svc=joblib.load('svc.sav')
+ann=joblib.load('ann.sav')
+main_bg = "nature-3267579_1920.jpg"
+main_bg_ext = "jpg"
+
+side_bg = "nature-3267579_1920.jpg"
+side_bg_ext = "jpg"
+
+st.markdown(
+    f"""
+    <style>
+    .reportview-container {{
+        background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()})
+    }}
+   .sidebar .sidebar-content {{
+        background: url(data:image/{side_bg_ext};base64,{base64.b64encode(open(side_bg, "rb").read()).decode()})
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+st.title('Water Potability Prediction !!!!!!!')
+st.subheader('Predict the water you drink is pure or not ??')
+
+         
+st.image('water.jpg')
+st.dataframe(data.head(200))
+header = st.container()
+body = st.container()
+with header:
+    col1,col2 = st.columns(2)
+    plot_type = col1.selectbox('Plot the feature',['histogram','line plot','area chart'])
+    feat = col1.selectbox('Which feature', ['ph','Turbidity'])
+    if plot_type == 'histogram':
+        col2.bar_chart(data[feat][:200])
+        col1.header(f'{plot_type} of {feat} feature')
+    if plot_type == 'line plot':
+        col2.line_chart(data[feat][:200])
+        col1.header(f'{plot_type} of {feat} feature')
+    if plot_type == 'area chart':
+        col2.area_chart(data[feat][:200])
+        col1.header(f'{plot_type} of {feat} feature')
+with body:
+    col1, col2 = st.columns(2)
+    col1.header('About Me')
+    col1.markdown('''My name is Yukta Lalwani , I'm pursuing my Computer Science Degree and love to do Machine Learning stuff''')
+    col2.header('About Project')
+    col2.markdown("Are you researching for water potability check, Just fill the content of water and here you go")
+
